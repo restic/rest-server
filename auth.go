e85e6fab962ef95e82b5c93e7d4847f59ccd80f2
Chapuis Bertil <bchapuis@agimem.com>
2015-09-06 17:45:08 +0200

    small refactoring

--- auth.go
+++ auth.go
@@ -11,11 +11,6 @@ func Authorize(r *http.Request) error {
 		return errors.New("malformed basic auth credentials")
 	}
 
-	repo, err := RepositoryName(r.RequestURI)
-	if err != nil {
-		return err
-	}
-
 	if username != "user" || password != "pass" {
 		return errors.New("unknown user")
 	}
