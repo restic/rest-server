ebe3bc04b5165e3b68873e97763d2e63549e178d
Zlatko Čalušić <zcalusic@bitsync.net>
2017-10-18 23:18:44 +0200

    Docker: various updates
    - use exec in entrypoint.sh, get rid of extra shell process
    - use CMD instead of ENTRYPOINT, so image can be run with /bin/sh arg

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if [ -z "$DISABLE_AUTHENTICATION" ]; then
+    if [ ! -f "$PASSWORD_FILE" ]; then
+        touch "$PASSWORD_FILE"
+    fi
+
+    if [ ! -s "$PASSWORD_FILE" ]; then
+        echo
+        echo "**WARNING** No user exists, please 'docker exec -it \$CONTAINER_ID create_user'"
+        echo
+    fi
+else
+    rm -f "$PASSWORD_FILE"
+fi
+
+exec rest-server --listen ":80" $OPTIONS --path "$DATA_DIRECTORY"
